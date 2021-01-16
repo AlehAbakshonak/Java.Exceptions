cb91f072d714028cb4ec9efa4ee82c7eb631d6f8
Aleh Abakshonak <abakshonokoleg@gmail.com>
2021-01-16 12:26:15 +0300

    updated try..catch in runner

--- src/main/java/com/epam/abak/javaExceptions/Runner.java
+++ src/main/java/com/epam/abak/javaExceptions/Runner.java
@@ -9,48 +9,50 @@ import com.epam.abak.javaExceptions.universityTree.University;
 import com.epam.abak.javaExceptions.userExceptions.*;
 
 public class Runner {
-   public static void main(String[] args)
-         throws Exception {
-      University hogwarts = University.createNewUniversity("Hogwarts");
-
-      Faculty[] faculties = hogwarts.addNewFaculties(new String[]{"Gryffindor", "Slytherin"});
-
-      Group[][] groups = new Group[][]{
-            faculties[0].addNewGroups(new String[]{"Gry Year 1", "Gry Year 2"}),
-            faculties[1].addNewGroups(new String[]{"Sly Year 1", "Sly Year 2"}),
-      };
-
-      Student[][][] students = new Student[][][]{
-            {
-                  groups[0][0].addNewStudents(
-                        new String[]{"Gry Year 1 Stud"},
-                        new int[][][]{
-                              {{0, 2}, {1, 4}, {3, 6}}
-                        }
-                  ),
-                  groups[0][1].addNewStudents(
-                        new String[]{"Gry Year 2 Stud"},
-                        new int[][][]{
-                              {{2, 8}, {3, 10}}
-                        }
-                  )
-            },
-            {
-                  groups[1][0].addNewStudents(
-                        new String[]{"Sly Year 1 Stud"},
-                        new int[][][]{
-                              {{0, 1}, {1, 3}, {3, 5}}
-                        }
-                  ),
-                  groups[1][1].addNewStudents(
-                        new String[]{"Sly Year 2 Stud"},
-                        new int[][][]{
-                              {{1, 7}, {2, 9}}
-                        }
-                  )
-            }
-      };
+   public static void main(String[] args) {
+      University hogwarts;
+      Faculty[] faculties;
+      Group[][] groups;
+      Student[][][] students;
       try {
+         hogwarts = University.createNewUniversity("Hogwarts");
+         faculties = hogwarts.addNewFaculties(new String[]{"Gryffindor", "Slytherin"});
+
+         groups = new Group[][]{
+               faculties[0].addNewGroups(new String[]{"Gry Year 1", "Gry Year 2"}),
+               faculties[1].addNewGroups(new String[]{"Sly Year 1", "Sly Year 2"}),
+         };
+
+         students = new Student[][][]{
+               {
+                     groups[0][0].addNewStudents(
+                           new String[]{"Gry Year 1 Stud"},
+                           new int[][][]{
+                                 {{0, 2}, {1, 4}, {3, 6}}
+                           }
+                     ),
+                     groups[0][1].addNewStudents(
+                           new String[]{"Gry Year 2 Stud"},
+                           new int[][][]{
+                                 {{2, 8}, {3, 10}}
+                           }
+                     )
+               },
+               {
+                     groups[1][0].addNewStudents(
+                           new String[]{"Sly Year 1 Stud"},
+                           new int[][][]{
+                                 {{0, 1}, {1, 3}, {3, 5}}
+                           }
+                     ),
+                     groups[1][1].addNewStudents(
+                           new String[]{"Sly Year 2 Stud"},
+                           new int[][][]{
+                                 {{1, 7}, {2, 9}}
+                           }
+                     )
+               }
+         };
          Student currentStudent = students[0][0][0];
          System.out.printf("Average mark of student %s (Group %s of faculty %s): %.0f\n",
                currentStudent.getStudentName(),
@@ -81,12 +83,13 @@ public class Runner {
                hogwarts.getUniversityName(),
                hogwarts.countAverageMarkInDisciplineById(2)
          );
-      }
-      catch (EmptyEducationalUnitException |
+      } catch (EmptyEducationalUnitException |
             WrongDisciplineIndexException |
             WrongMarksAmountException |
             IllegalMarkValueException |
             EmptyNameException ex) {
+         System.out.println(ex.getClass().getName()+": "+ex.getMessage());
+      } catch (Exception ex) {
          System.out.println(ex.getMessage());
       }
    }
